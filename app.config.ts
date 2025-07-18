a61093882ade10aac84561ba7eaa5ff35c27e63c
nino-mau <nino.mauuary@gmail.com>
2025-07-19 00:44:39 +0200

    Feat(nuxtui): Change icon size for Input global style

--- app.config.ts
+++ app.config.ts
@@ -6,6 +6,21 @@ export default defineAppConfig({
       tertiary: 'tertiary',
     },
 
+    input: {
+      variants: {
+        size: {
+          xl: {
+            base: 'px-3 py-2 text-base gap-2',
+            leading: 'ps-3',
+            trailing: 'pe-3',
+            leadingIcon: 'size-5.5',
+            leadingAvatarSize: 'xs',
+            trailingIcon: 'size-5.5',
+          },
+        },
+      },
+    },
+
     button: {
       slots: {
         // Change border radius to sm for all buttons
@@ -17,30 +32,6 @@ export default defineAppConfig({
 
       variants: {
         size: {
-          xs: {
-            base: 'px-2 py-1 text-xs gap-1',
-            leadingIcon: 'size-4',
-            leadingAvatarSize: '3xs',
-            trailingIcon: 'size-4',
-          },
-          sm: {
-            base: 'px-2.5 py-1.5 text-xs gap-1.5',
-            leadingIcon: 'size-4',
-            leadingAvatarSize: '3xs',
-            trailingIcon: 'size-4',
-          },
-          md: {
-            base: 'px-2.5 py-1.5 text-sm gap-1.5',
-            leadingIcon: 'size-5',
-            leadingAvatarSize: '2xs',
-            trailingIcon: 'size-5',
-          },
-          lg: {
-            base: 'px-3 py-2 text-sm gap-2',
-            leadingIcon: 'size-5',
-            leadingAvatarSize: '2xs',
-            trailingIcon: 'size-5',
-          },
           xl: {
             base: 'px-3 py-2 text-base gap-2',
             leadingIcon: 'size-5.5',
