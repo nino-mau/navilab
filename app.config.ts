30ef675c69e6d274a9544bce2819b39e812bf6b0
nino-mau <nino.mauuary@gmail.com>
2025-07-18 22:32:50 +0200

    Feat(nuxtui): Change the icon size for Button global style

--- app.config.ts
+++ app.config.ts
@@ -14,6 +14,43 @@ export default defineAppConfig({
           'transition-colors',
         ],
       },
+
+      variants: {
+        size: {
+          xs: {
+            base: 'px-2 py-1 text-xs gap-1',
+            leadingIcon: 'size-4',
+            leadingAvatarSize: '3xs',
+            trailingIcon: 'size-4',
+          },
+          sm: {
+            base: 'px-2.5 py-1.5 text-xs gap-1.5',
+            leadingIcon: 'size-4',
+            leadingAvatarSize: '3xs',
+            trailingIcon: 'size-4',
+          },
+          md: {
+            base: 'px-2.5 py-1.5 text-sm gap-1.5',
+            leadingIcon: 'size-5',
+            leadingAvatarSize: '2xs',
+            trailingIcon: 'size-5',
+          },
+          lg: {
+            base: 'px-3 py-2 text-sm gap-2',
+            leadingIcon: 'size-5',
+            leadingAvatarSize: '2xs',
+            trailingIcon: 'size-5',
+          },
+          xl: {
+            base: 'px-3 py-2 text-base gap-2',
+            leadingIcon: 'size-5.5',
+            leadingAvatarSize: 'xs',
+            trailingIcon: 'size-5.5',
+          },
+        },
+      },
+
+      compoundVariants: [],
     },
   },
 });
