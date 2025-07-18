d37936d97da89c2894ec0cea09dd25783ad4d1fe
nino-mau <nino.mauuary@gmail.com>
2025-07-18 20:38:35 +0200

    Feat(nuxtui): Edit nuxtUi button component default theme
    Change border-radius for nuxtUi button component from md to sm

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,17 @@ export default defineAppConfig({
     colors: {
       primary: 'primary',
       secondary: 'secondary',
+      tertiary: 'tertiary',
+    },
+
+    button: {
+      slots: {
+        // Change border radius to sm for all buttons
+        base: [
+          'rounded-sm font-medium inline-flex items-center disabled:cursor-not-allowed aria-disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:opacity-75',
+          'transition-colors',
+        ],
+      },
     },
   },
 });
