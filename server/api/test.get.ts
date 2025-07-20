6c7313cdb7cd2fc9503ff98e5f9b4311a1d13786
nino-mau <nino.mauuary@gmail.com>
2025-07-20 22:48:30 +0200

    Chore: remove unused test ts files

--- server/api/test.get.ts
+++
@@ -1,5 +0,0 @@
-import { showTables } from '../services/test';
-
-export default defineEventHandler(async () => {
-  return await showTables();
-});
--- server/services/test.ts
+++
@@ -1,9 +0,0 @@
-import { sql } from 'drizzle-orm';
-import db from '../db/client';
-
-export async function showTables() {
-  const result = await db.execute(
-    sql`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`
-  );
-  return result;
-}
