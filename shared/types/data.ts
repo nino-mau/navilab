73ae6ee3b243f1c4e97fddd6e526c4af73560f2a
nino-mau <nino.mauuary@gmail.com>
2025-07-22 18:35:52 +0200

    Feat(types): Create ts data types

---
+++ shared/types/data.ts
@@ -0,0 +1,5 @@
+export type DetectorRole = 'Human' | 'Audio' | 'Video';
+
+export type DetectorStatus = 'online' | 'offline' | 'inactive';
+
+export type UserRole = 'Product Manager' | 'Contributor' | 'Admin';
