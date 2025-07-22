eb551974348845663ec2d840def9ba8671809c78
nino-mau <nino.mauuary@gmail.com>
2025-07-22 23:43:04 +0200

    Refactor(api): Change error handling strategy on api

--- server/api/user/[id].ts
+++ server/api/user/[id].ts
@@ -4,18 +4,18 @@ export default defineEventHandler(async (event) => {
   const userId = getRouterParam(event, 'id');
 
   if (!userId)
-    return sendError(
-      event,
-      createError({ statusCode: 400, statusMessage: 'Missing userId' })
-    );
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Missing userId'
+    });
 
   const userData = await fetchUserById(userId);
 
   if (!userData || !userData.length)
-    return sendError(
-      event,
-      createError({ statusCode: 404, statusMessage: 'User not found' })
-    );
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'User not found'
+    });
 
   return userData;
 });
