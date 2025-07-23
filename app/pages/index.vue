c49d3f48125437d36fc724b36436c867e2623bac
nino-mau <nino.mauuary@gmail.com>
2025-07-24 00:08:14 +0200

    Feat(ui): Change style of homepage header and hover effects of homepage buttons

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="mx-6 flex flex-col gap-10">
     <!-- Card: Welcome Heading -->
-    <div
-      class="bg-secondary mt-10 flex flex-col gap-4 rounded-md p-8 shadow-md"
-    >
-      <h1 class="text-4xl font-bold text-white">Welcome To Navilab</h1>
-      <p class="text-xl font-medium text-white opacity-80">
+    <div class="card mt-10 flex flex-col gap-4 rounded-md p-8 shadow-md">
+      <h1 class="text-default text-4xl font-bold">Welcome To Navilab</h1>
+      <p class="text-text2 text-xl font-medium opacity-80">
         Lorem ipsum lorem ipsum lorem
       </p>
     </div>
@@ -14,31 +12,35 @@
       class="grid grow grid-cols-[1fr_1fr_1fr] grid-rows-[230px_230px] gap-x-6 gap-y-6"
     >
       <!-- Link: Projects -->
-      <button
+      <UButton
+        variant="link"
         :class="
           cn(
-            'bg-primary col-span-1 row-span-1 flex cursor-pointer flex-col items-center justify-center gap-3 rounded-md shadow-md',
-            'hover:bg-primary/90 hvr-float !transition-all !duration-250 !ease-out'
+            'hover:bg-primary/90 hvr-grow-big !flex !transition-all !duration-250 !ease-out',
+            'bg-primary col-span-1 row-span-1 cursor-pointer flex-col items-center justify-center gap-4 rounded-md shadow-md'
           )
         "
+        to="/projects"
         aria-label="Go to projects page"
       >
-        <LucideMap :size="80" class="stroke-[1.2]" />
+        <LucideLayers :size="80" class="stroke-[1.2]" />
         <h2 class="text-2xl font-semibold text-white">Projects</h2>
-      </button>
+      </UButton>
       <!-- Link: Detectors -->
-      <button
+      <UButton
+        variant="link"
         :class="
           cn(
-            'bg-primary col-span-1 row-span-1 flex cursor-pointer flex-col items-center justify-center gap-3 rounded-md shadow-md',
-            'hover:bg-primary/90 hvr-float !transition-all !duration-250 !ease-out'
+            'hover:bg-primary/90 hvr-grow-big !flex !transition-all !duration-250 !ease-out',
+            'bg-primary col-span-1 row-span-1 cursor-pointer flex-col items-center justify-center gap-3 rounded-md shadow-md'
           )
         "
+        to="/detectors"
         aria-label="Go to detectors page"
       >
-        <LucideRadio :size="80" class="stroke-[1.2]" />
+        <LucideRadar :size="80" class="stroke-[1.2]" />
         <h2 class="text-2xl font-semibold text-white">Detectors</h2>
-      </button>
+      </UButton>
     </div>
   </div>
 </template>
