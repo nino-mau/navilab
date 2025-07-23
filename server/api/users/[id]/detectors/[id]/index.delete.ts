7f957f2adb522628d7ab152d4d6db8aed7ad4c3e
nino-mau <nino.mauuary@gmail.com>
2025-07-23 17:24:14 +0200

    Feat(api): Create endpoint to delete a detector by id

---
+++ server/api/users/[id]/detectors/[id]/index.delete.ts
@@ -0,0 +1,30 @@
+import { deleteDetectorById } from '~~/server/services/detector.service';
+
+/**
+ * Delete a detector from database
+ *
+ * @DELETE /api/users/[id]/detectors/[id]
+ */
+export default defineEventHandler(async (event) => {
+  const detectorId = getRouterParam(event, 'id');
+
+  if (!detectorId) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Missing detectorId'
+    });
+  }
+
+  // Delete from db
+  const res = await deleteDetectorById(detectorId);
+
+  if (!res.length || !res[0].deletedId) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Failed to delete detector from database'
+    });
+  }
+
+  setResponseStatus(event, 204);
+  return { deleteId: res[0].deletedId };
+});
