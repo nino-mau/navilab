a3e0c0fd0859d9f26dc45d0133db3ab42e51663a
nino-mau <nino.mauuary@gmail.com>
2025-07-24 21:37:36 +0200

    Feat(plugin): Create plugin that create an admin user on initialization

---
+++ server/plugins/createAdmin.ts
@@ -0,0 +1,20 @@
+import { eq } from 'drizzle-orm';
+import db from '../db/client';
+import { user } from '../db/schema';
+
+export default defineNitroPlugin(async () => {
+  const existing = await db
+    .select()
+    .from(user)
+    .where(eq(user.email, 'admin@t.com'));
+  if (existing.length === 0) {
+    await auth.api.createUser({
+      body: {
+        email: 'admin@t.com',
+        password: '118234',
+        name: 'admin',
+        role: 'Admin' as any // eslint-disable-line
+      }
+    });
+  }
+});
