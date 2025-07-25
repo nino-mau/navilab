095ddec8a52ae9ce5c2fc43839b306dec906c5cb
nino-mau <nino.mauuary@gmail.com>
2025-07-25 20:33:24 +0200

    Feat(seeding): Create realistic placeholders data for project, users and detectors

--- server/db/seed-placeholder.ts
+++ server/db/seed-placeholder.ts
@@ -1,16 +1,53 @@
-import { eq } from 'drizzle-orm';
 import 'dotenv/config';
-import projectsPlaceholderData from './data/placeholderProjects.json';
-import { project, user } from './schema';
+import { eq } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import projectsData from './data/placeholder/projects.json';
+import usersData from './data/placeholder/users.json';
+import detectorsData from './data/placeholder/detectors.json';
+import projectContributorsData from './data/placeholder/projectContributors.json';
+import projectDetectorsData from './data/placeholder/projectDetectors.json';
+import {
+  detector,
+  project,
+  projectContributors,
+  projectDetectors,
+  user
+} from './schema';
 
 /**
- * Insert placeholder data
+ * Insert placeholder data in db
  */
 async function main() {
   const db = drizzle(process.env.DATABASE_URL!);
-  await db.execute(`TRUNCATE TABLE "project" RESTART IDENTITY CASCADE;`);
 
+  // Clear previous data
+  await db.execute(`TRUNCATE TABLE "project" RESTART IDENTITY CASCADE;`);
+  await db.execute(`TRUNCATE TABLE "detector" RESTART IDENTITY CASCADE;`);
+  await db.execute(
+    `TRUNCATE TABLE "project_detectors" RESTART IDENTITY CASCADE;`
+  );
+  await db.execute(
+    `TRUNCATE TABLE "project_contributors" RESTART IDENTITY CASCADE;`
+  );
+
+  // Insert placeholder users
+  await db.insert(user).values(
+    usersData.map((u) => ({
+      ...u,
+      role: u.role as 'Contributor' | 'Product Manager' | 'Admin'
+    }))
+  );
+
+  // Insert placeholder detectors
+  await db.insert(detector).values(
+    detectorsData.map((d) => ({
+      ...d,
+      type: d.type as 'Human' | 'Audio' | 'Video',
+      status: d.status as 'online' | 'offline' | 'inactive'
+    }))
+  );
+
+  // Insert placeholder projects on admin user
   const adminId = await db
     .select({ id: user.id })
     .from(user)
@@ -19,11 +56,15 @@ async function main() {
   if (!adminId[0].id) console.log('No admin user found');
 
   await db.insert(project).values(
-    projectsPlaceholderData.map((project) => ({
+    projectsData.map((project) => ({
       ...project,
       managerId: adminId[0].id
     }))
   );
+
+  // Insert data for project detectors and contributors
+  await db.insert(projectContributors).values(projectContributorsData);
+  await db.insert(projectDetectors).values(projectDetectorsData);
 
   console.log('✅ Seed Placeholder complete');
   return;
