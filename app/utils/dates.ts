8b2f842b9992706704c27e38cfd482069dd1b0a7
nino-mau <nino.mauuary@gmail.com>
2025-07-26 19:52:30 +0200

    Feat(utils): Create helpers functions to calculate time values

--- app/utils/dates.ts
+++ app/utils/dates.ts
@@ -1,4 +1,6 @@
 import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/en';
 
 /**
  * Calculate the time since a given date, return non verbose result
@@ -27,4 +29,47 @@ export function timeSinceDate(date: string): string {
   } else {
     return diffSeconds + ` second${diffSeconds > 1 ? 's' : ''} ago`;
   }
+}
+
+/**
+ * Calculate the remaining time between now to a specific end date
+ */
+export function timeRemaining(endDate: string) {
+  dayjs.extend(relativeTime);
+  dayjs.locale('en', {
+    relativeTime: {
+      future: '%s remains',
+      past: '%s ago',
+      s: '1 second',
+      m: '1 minute',
+      mm: '%d minutes',
+      h: '1 hour',
+      hh: '%d hours',
+      d: '1 day',
+      dd: '%d days',
+      M: '1 month',
+      MM: '%d months',
+      y: '1 year',
+      yy: '%d years'
+    }
+  });
+  return dayjs().to(endDate);
+}
+
+/**
+ * Calculate the percentage of progress between two dates based on current date
+ */
+export function progressBetweenDates(
+  startDate: string,
+  endDate: string
+): number {
+  const current = dayjs();
+  const end = dayjs(endDate);
+  const start = dayjs(startDate);
+  const duration = end.diff(start);
+  const elapsed = current.diff(start);
+
+  if (current > end || current < start) return 0;
+
+  return elapsed / (duration / 100);
 }
