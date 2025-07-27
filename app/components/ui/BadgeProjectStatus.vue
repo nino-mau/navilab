9722914a77d1c82915198f7689e5ef7b7a86f18c
nino-mau <nino.mauuary@gmail.com>
2025-07-27 15:22:52 +0200

    Feat(components): Create custom badge and chip component to handle displaying project status

---
+++ app/components/ui/BadgeProjectStatus.vue
@@ -0,0 +1,38 @@
+<template>
+  <template v-if="props.status === 'not started'">
+    <UBadge
+      :label="capitalizeFirstLetter(props.status)"
+      size="md"
+      color="neutral"
+      class="bg-slate-400"
+      variant="solid"
+    />
+  </template>
+  <template v-if="props.status === 'in progress'">
+    <UBadge
+      :label="capitalizeFirstLetter(props.status)"
+      size="md"
+      color="warning"
+      variant="subtle"
+    />
+  </template>
+  <template v-if="props.status === 'finished'">
+    <UBadge
+      :label="capitalizeFirstLetter(props.status)"
+      size="md"
+      color="success"
+      variant="subtle"
+    />
+  </template>
+</template>
+
+<script setup lang="ts">
+import { capitalizeFirstLetter } from 'better-auth';
+
+const props = defineProps({
+  status: {
+    type: String as PropType<ProjectStatus>,
+    required: true
+  }
+});
+</script>
---
+++ app/components/ui/ChipProjectStatus.vue
@@ -0,0 +1,25 @@
+<template>
+  <template v-if="props.status === 'not started'">
+    <UChip
+      size="xl"
+      color="neutral"
+      class="mt-1 bg-slate-400"
+      variant="solid"
+    />
+  </template>
+  <template v-if="props.status === 'in progress'">
+    <UChip size="xl" class="mt-1" color="warning" variant="subtle" />
+  </template>
+  <template v-if="props.status === 'finished'">
+    <UChip size="xl" class="mt-1" color="success" variant="subtle" />
+  </template>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  status: {
+    type: String as PropType<ProjectStatus>,
+    required: true
+  }
+});
+</script>
