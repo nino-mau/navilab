e86b8f1b457ca77f05fe5dbbc0df62de7dbe82f3
nino-mau <nino.mauuary@gmail.com>
2025-07-28 00:44:15 +0200

    Refactor(utils): Refactor helper function timeSinceDate to use dayjs relativeTime plugin

--- app/utils/dates.ts
+++ app/utils/dates.ts
@@ -6,29 +6,25 @@ import 'dayjs/locale/en';
  * Calculate the time since a given date, return non verbose result
  */
 export function timeSinceDate(date: string): string {
-  const now = dayjs();
-  const since = dayjs(date);
-
-  const diffSeconds = now.diff(since, 'second');
-  const diffMinutes = now.diff(since, 'minute');
-  const diffHours = now.diff(since, 'hour');
-  const diffDays = now.diff(since, 'day');
-  const diffMonths = now.diff(since, 'month');
-  const diffYears = now.diff(since, 'year');
-
-  if (diffYears >= 1) {
-    return diffYears + ` year${diffYears > 1 ? 's' : ''} ago`;
-  } else if (diffMonths >= 1) {
-    return diffMonths + ` month${diffMonths > 1 ? 's' : ''} ago`;
-  } else if (diffDays >= 1) {
-    return diffDays + ` day${diffDays > 1 ? 's' : ''} ago`;
-  } else if (diffHours >= 1) {
-    return diffHours + ` hour${diffHours > 1 ? 's' : ''} ago`;
-  } else if (diffMinutes >= 1) {
-    return diffMinutes + ` minute${diffMinutes > 1 ? 's' : ''} ago`;
-  } else {
-    return diffSeconds + ` second${diffSeconds > 1 ? 's' : ''} ago`;
-  }
+  dayjs.extend(relativeTime);
+  dayjs.locale('en', {
+    relativeTime: {
+      future: '%s ago',
+      past: '%s ago',
+      s: '1 second',
+      m: '1 minute',
+      mm: '%d minutes',
+      h: '1 hour',
+      hh: '%d hours',
+      d: '1 day',
+      dd: '%d days',
+      M: '1 month',
+      MM: '%d months',
+      y: '1 year',
+      yy: '%d years'
+    }
+  });
+  return dayjs(date).to(dayjs());
 }
 
 /**
