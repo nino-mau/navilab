2d61a4d471988dc1f7cba4ae95cde954922f3c84
nino-mau <nino.mauuary@gmail.com>
2025-07-28 14:01:38 +0200

    Feat(stores): Create the requests store and a fetch action

---
+++ app/stores/requests.ts
@@ -0,0 +1,36 @@
+export const useRequestsStore = defineStore('requestsStore', {
+  state: () => ({
+    requests: [] as RequestClient[]
+  }),
+  actions: {
+    async fetch(userId: string) {
+      const res = await $fetch.raw(`/api/users/${userId}/requests`, {
+        method: 'GET'
+      });
+
+      if (!res.ok) {
+        return;
+      }
+
+      this.requests = res._data ?? [];
+    }
+
+    // async delete(detectorId: string, userId: string) {
+    //   const res = await $fetch.raw(
+    //     `/api/users/${userId}/detectors/${detectorId}`,
+    //     {
+    //       method: 'DELETE'
+    //     }
+    //   );
+    //
+    //   if (!res.ok) {
+    //     return;
+    //   }
+    //
+    //   // Remove the deleted detector from the state
+    //   this.detectors = this.detectors.filter(
+    //     (detector) => detector.id !== detectorId
+    //   );
+    // }
+  }
+});
