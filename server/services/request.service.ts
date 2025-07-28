843dd05a5953c169ec83e352158d10a813736c12
nino-mau <nino.mauuary@gmail.com>
2025-07-28 14:00:08 +0200

    Feat(services): Add new fields to the data extracted by fetchRequestsById

--- server/services/request.service.ts
+++ server/services/request.service.ts
@@ -1,15 +1,23 @@
 import { eq, getTableColumns } from 'drizzle-orm';
 import db from '../db/client';
-import { project, projectRequest } from '../db/schema';
+import { project, projectRequest, user } from '../db/schema';
+import { getISOFormatDateQuery } from '../utils/db';
 
 /**
  * Fetch  requests to all projects of a user by user id
  */
 export async function fetchRequestsById(userId: string) {
   const res = db
-    .select({ ...getTableColumns(projectRequest) })
+    .select({
+      ...getTableColumns(projectRequest),
+      createdAt: getISOFormatDateQuery(projectRequest.createdAt),
+      projectName: project.name,
+      requesterName: user.name,
+      requesterEmail: user.email
+    })
     .from(project)
     .innerJoin(projectRequest, eq(project.id, projectRequest.projectId))
+    .innerJoin(user, eq(projectRequest.requesterId, user.id))
     .where(eq(project.managerId, userId));
   return res;
 }
