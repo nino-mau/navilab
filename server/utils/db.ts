5840a0e1917b4ff58d67046d1c9554fee92797c9
nino-mau <nino.mauuary@gmail.com>
2025-07-28 13:59:13 +0200

    Feat(utils): Create helper function to transform date column to string in drizzle queries

---
+++ server/utils/db.ts
@@ -0,0 +1,7 @@
+import type { SQL } from 'drizzle-orm';
+import { sql } from 'drizzle-orm';
+import type { PgColumn } from 'drizzle-orm/pg-core';
+
+export function getISOFormatDateQuery(dateTimeColumn: PgColumn): SQL<string> {
+  return sql<string>`to_char(${dateTimeColumn}, 'YYYY-MM-DD"T"HH24:MI:SS"Z"')`;
+}
