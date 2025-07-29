2d3456f2c1ba1146a2eaf2da32c8d902d1769057
nino-mau <nino.mauuary@gmail.com>
2025-07-29 21:53:29 +0200

    Feat(component): Create a reuasable empty state component

---
+++ app/components/ui/EmptyState.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="my-auto flex flex-col items-center">
+    <div
+      class="bg-tertiary flex size-16 items-center justify-center rounded-sm"
+    >
+      <component :is="props.icon" v-if="props.icon" :size="32" />
+    </div>
+
+    <div class="mt-3 flex flex-col items-center gap-1">
+      <!-- Label -->
+      <h2 class="text-default text-xl font-semibold">{{ props.label }}</h2>
+      <!-- Description -->
+      <p class="text-dimmed max-w-[400px] text-center">{{ props.desc }}</p>
+    </div>
+
+    <UButton
+      variant="outline"
+      color="neutral"
+      :label="props.buttonLabel"
+      size="xl"
+      class="mt-6"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  label?: string;
+  buttonLabel?: string;
+  desc?: string;
+  icon: LucideIcon;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  label: 'No Projects',
+  buttonLabel: 'Create Project',
+  desc: 'You currently do not have any project, create one to start using our features'
+});
+</script>
