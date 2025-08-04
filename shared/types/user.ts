1eb15f259b4b2edfc5ff89ca8291c6d7dd6cc904
nino-mau <nino.mauuary@gmail.com>
2025-08-05 00:00:11 +0200

    Feat(types): Add a User type

--- shared/types/user.ts
+++ shared/types/user.ts
@@ -1,1 +1,6 @@
+import type { InferSelectModel } from 'drizzle-orm';
+import type { user } from '~~/server/db/schema';
+
+export type User = InferSelectModel<typeof user>;
+
 export type UserRole = 'Product Manager' | 'Contributor' | 'Admin';
